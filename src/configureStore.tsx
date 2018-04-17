b1e9aff95ed0c60ea537832cc09b577470b2af03
Kwangje Cho <kjcho@tapyn.com>
2018-04-17 09:20:38 +0900

    update

---
+++ src/configureStore.tsx
@@ -0,0 +1,25 @@
+import { History } from 'history';
+import { routerMiddleware } from 'react-router-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducers from './rootReducers';
+import rootSagas from './rootSagas';
+
+function configureStore(history: History) {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, routerMiddleware(history)];
+
+  const store = createStore(
+    rootReducers,
+    window.devToolsExtension && process.env.NODE_ENV !== 'production'
+      ? compose(applyMiddleware(...middlewares), window.devToolsExtension())
+      : applyMiddleware(...middlewares)
+  );
+
+  sagaMiddleware.run(rootSagas);
+
+  return store;
+}
+
+export default configureStore;
